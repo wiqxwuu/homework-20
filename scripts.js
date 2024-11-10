4e177e7ac21ee69d51939936c5770c8a90bc004a
Asema <asema.abdurasulova@gmail.com>
2024-11-11 04:48:19 +0600

    created async function get users

--- scripts.js
+++ scripts.js
@@ -1,1 +1,17 @@
 const box = document.querySelector('.content')
+const API = 'https://dummyjson.com/users'
+
+async function getUsers() {
+    try {
+        const res = await fetch(API)
+        const data = await res.json()
+
+        console.log(data);
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
+
+getUsers()
